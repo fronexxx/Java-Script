890e0e9255808766475745eca2cea88431d40a55
fronexxx <igorbotsii42@gmail.com>
2025-05-03 14:13:35 +0300

    HW_1

---
+++ Classwork/Lesson 1/main.js
@@ -0,0 +1,14 @@
+// var a;
+// a = 'hello world';
+// console.log(a);
+
+// let a = 'hello world';
+// console.log(a);
+
+const PI = 3.14;
+console.log(PI);
+
+let a = 'hgahsghga'
+console.log(a)
+a = 'okteeen'
+console.log(a)
