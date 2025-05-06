3dcd24baeee061b8ae72f05f7e700ca50534e954
fronexxx <igorbotsii42@gmail.com>
2025-05-06 20:55:25 +0300

    Aditional HW

---
+++ Aditional HW/Aditional_HW_3/Task 1-2/main.js
@@ -0,0 +1,19 @@
+// Task-1
+
+let arr = [5, 10, 15, 14, 32];
+console.log(arr);
+
+let arr2 = ['asd', 'dsa', 'ghsadv', 'bcvb', 'hsgdh'];
+console.log(arr2);
+
+let arr3 = [
+    [1, 2, 32, 14, 56],
+    ['frame', 'bob', 'hsgdhja', 'bcvb', 'hsgdh'],
+    [true, false, 10 === 10, 14 > 2, 10 !== 10]
+];
+console.log(arr3);
+
+// Task-2
+
+let arr4 = [5, 10, 15, 14, 32];
+console.log(arr4[3]);
