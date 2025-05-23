c500247984e1c6060647b16778611b475f263ad8
fronexxx <igorbotsii42@gmail.com>
2025-05-23 13:00:17 +0300

    hw10

--- Homeworks/HW_10/Task-9/main.js
+++ Homeworks/HW_10/Task-9/main.js
@@ -1,15 +1,32 @@
 // Task-9
 
-const now = Date.now();
-let price = parseInt(localStorage.getItem('price')) || 100;
-const lastUpdate = parseInt(localStorage.getItem('lastUpdate')) || 0;
+// const now = Date.now();
+// let price = parseInt(localStorage.getItem('price')) || 100;
+// const lastUpdate = parseInt(localStorage.getItem('lastUpdate')) || 0;
+//
+//
+// if (now - lastUpdate >= 10000) {
+//     price += 10;
+//     localStorage.setItem('price', price);
+//     localStorage.setItem('lastUpdate', now);
+// }
+//
+// document.getElementById('price').innerText = price + ' UAH';
 
+
+let now = new Date().getTime();
+
+let target = document.getElementById('target').innerText;
+let number = parseInt(target);
+
+
+let price = parseInt(localStorage.getItem('productPrice')) || number;
+let lastUpdate = parseInt(localStorage.getItem('lastUpdate')) || 0;
 
 if (now - lastUpdate >= 10000) {
     price += 10;
-    localStorage.setItem('price', price);
+    localStorage.setItem('productPrice', price);
     localStorage.setItem('lastUpdate', now);
 }
 
-document.getElementById('price').textContent = price + ' UAH';
-
+document.getElementById('target').innerText = price + ' UAH';
